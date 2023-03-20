37646f83eea6754fb44e4461dba694c723dc3300
Joe Burgess <josephburgess@gmail.com>
2023-03-20 14:30:06 +0000

    adding scraper classes

---
+++ src/scraper.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+class Scraper:
+    def fetch_html(self, url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.text
+        return None
+
+    def scrape(self):
+        raise NotImplementedError("Subclasses must implement this method")
+
+
+class PubMedScraper(Scraper):
+    def parse_pubmed(self, html_content):
+        soup = BeautifulSoup(html_content, 'html.parser')
+        articles = []
+
+        return articles
+
+    def scrape(self):
+        url = 'https://pubmed.ncbi.nlm.nih.gov/?term=long+covid'
+        html_content = self.fetch_html(url)
+        if html_content:
+            return self.parse_pubmed(html_content)
+        return []
---
+++ tests/test_scraper.py
@@ -0,0 +1,23 @@
+import pytest
+import responses
+from src.scraper import Scraper, PubMedScraper
+
+sample_pubmed_html = '''
+<div class="pubmed-docsum">
+    <a class="docsum-title" href="/12345678/">Sample Article Title</a>
+    <span class="docsum-authors">John Doe, Jane Smith</span>
+    <span class="docsum-journal-citation">Jan 01, 2022</span>
+</div>
+'''
+
+
+@responses.activate
+def test_fetch_html_pubmed():
+    url = 'https://pubmed.ncbi.nlm.nih.gov/?term=long+covid'
+    responses.add(responses.GET, url, body=sample_pubmed_html, status=200)
+
+    pubmed_scraper = PubMedScraper()
+    html_content = pubmed_scraper.fetch_html(url)
+
+    assert html_content is not None
+    assert 'pubmed-docsum' in html_content
