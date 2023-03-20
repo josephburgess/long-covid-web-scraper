ee8c640a391f52d88cecc7c70eeb4342d4682b95
Joe Burgess <josephburgess@gmail.com>
2023-03-20 15:57:32 +0000

    adding BMJ scraper

---
+++ src/scraper/bmj_scraper.py
@@ -0,0 +1,51 @@
+from scraper import Scraper
+from bs4 import BeautifulSoup
+
+
+class BMJScraper(Scraper):
+    def parse_bmj(self, html_content):
+        soup = BeautifulSoup(html_content, 'html.parser')
+        articles = []
+
+        article_containers = soup.find_all(
+            'div', class_='highwire-article-citation')
+
+        for container in article_containers:
+            title = container.find(
+                'a', class_='highwire-cite-linked-title').text.strip()
+
+            authors_list = container.find_all(
+                'span', {'class': ['nlm-given-names', 'nlm-surname']})
+            authors = ' '.join([author.text for author in authors_list])
+
+            publication_date = container.find(
+                'span', class_='highwire-cite-metadata-date').text.strip()
+
+            article = {
+                'title': title,
+                'authors': authors,
+                'publication_date': publication_date,
+                'source': 'BMJ'
+            }
+
+            articles.append(article)
+
+        return articles
+
+    def scrape(self, max_pages=5):
+
+        base_url = "https://www.bmj.com/search/advanced/"
+        query = "title%3Along%2Bcovid%20title_flags%3Amatch-all%20limit_from%3A1840-01-01%20limit_to%3A2023-03-20%20numresults%3A10%20sort%3Arelevance-rank%20format_result%3Astandard?page="
+        all_articles = []
+
+        for page_num in range(1, max_pages + 1):
+            url = f"{base_url}{query}{page_num}"
+            html_content = self.fetch_html(url)
+
+            if html_content:
+                articles = self.parse_bmj(html_content)
+                all_articles.extend(articles)
+            else:
+                break
+
+        return all_articles
