fbfc6cd2048b62bc1a940ff950137cee49e92cc7
Joe Burgess <josephburgess@gmail.com>
2023-03-20 14:32:28 +0000

    adding parsing logic

--- src/scraper.py
+++ src/scraper.py
@@ -19,6 +19,30 @@ class PubMedScraper(Scraper):
         soup = BeautifulSoup(html_content, 'html.parser')
         articles = []
 
+        # Find all article containers
+        article_containers = soup.find_all('div', class_='pubmed-docsum')
+
+        for container in article_containers:
+            # Extract the title
+            title = container.find('a', class_='docsum-title').text.strip()
+
+            # Extract authors
+            authors = container.find(
+                'span', class_='docsum-authors').text.strip()
+
+            # Extract the publication date
+            publication_date = container.find(
+                'span', class_='docsum-journal-citation').text.strip()
+
+            # Save the extracted data in a dictionary
+            article = {
+                'title': title,
+                'authors': authors,
+                'publication_date': publication_date
+            }
+
+            articles.append(article)
+
         return articles
 
     def scrape(self):
--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -21,3 +21,13 @@ def test_fetch_html_pubmed():
 
     assert html_content is not None
     assert 'pubmed-docsum' in html_content
+
+
+def test_parse_pubmed():
+    pubmed_scraper = PubMedScraper()
+    articles = pubmed_scraper.parse_pubmed(sample_pubmed_html)
+
+    assert len(articles) == 1
+    assert articles[0]['title'] == 'Sample Article Title'
+    assert articles[0]['authors'] == 'John Doe, Jane Smith'
+    assert articles[0]['publication_date'] == 'Jan 01, 2022'
