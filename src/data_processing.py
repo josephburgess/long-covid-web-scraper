5437f6f642024f98732859ab092b877b8f66de86
Joe Burgess <josephburgess@gmail.com>
2023-03-21 13:12:24 +0000

    adding data processing logic

---
+++ src/data_processing.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import re
+
+
+def load_data(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+
+def clean_data(df):
+    df = df.dropna()
+
+    for i, row in df.iterrows():
+        standardized_date = standardize_date(
+            row['publication_date'], row['source'])
+        df.at[i, 'publication_date'] = standardized_date
+
+    df = df.drop_duplicates(subset=['title'])
+    df = df.drop(columns=['source'])
+
+    return df
+
+
+def standardize_date(date_string, source):
+    if source == "pubmed":
+        date_match = re.search(r'(\d{4}) (\w{3}) (\d{2})', date_string)
+        if date_match:
+            year = date_match.group(1)
+            month = date_match.group(2)
+            day = date_match.group(3)
+            return f"{year}-{month}-{day}"
+    elif source == "BMJ":
+        date_match = re.search(
+            r'Published (\d{2}) (\w{3}) (\d{4})', date_string)
+        if date_match:
+            day = date_match.group(1)
+            month = date_match.group(2)
+            year = date_match.group(3)
+            return f"{year}-{month}-{day}"
+    return None
+
+
+def main():
+    file_path = 'data/raw/long_covid_articles.csv'
+
+    df = load_data(file_path)
+
+    processed_df = clean_data(df)
+
+    processed_df.to_csv('data/processed/processed_articles.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_data_processing.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import pytest
+from src.data_processing import load_data, clean_data, standardize_date
+
+
+@pytest.fixture
+def test_df():
+    df = pd.DataFrame({
+        'title': ['Article 1', 'Article 2', 'Article 3'],
+        'publication_date': ['2020 May 10', 'Published 15 Jun 2021', None],
+        'source': ['pubmed', 'BMJ', 'pubmed']
+    })
+    return df
+
+
+def test_load_data():
+    file_path = 'data/raw/test.csv'
+    df = load_data(file_path)
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) > 0
+
+
+def test_clean_data(test_df):
+    processed_df = clean_data(test_df)
+    assert len(processed_df) == 2
+    assert 'publication_date' in processed_df.columns
+    assert 'source' not in processed_df.columns
+    assert not processed_df.duplicated(subset=['title']).any()
+
+
+def test_standardize_date():
+    date1 = '2021 Aug 05'
+    source1 = 'pubmed'
+    assert standardize_date(date1, source1) == '2021-Aug-05'
+
+    date2 = 'Published 10 Mar 2022'
+    source2 = 'BMJ'
+    assert standardize_date(date2, source2) == '2022-Mar-10'
+
+    date3 = '2020 July 20'
+    source3 = 'other'
+    assert standardize_date(date3, source3) is None
