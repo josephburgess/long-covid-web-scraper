2022710bb634b035dae80bb6e369c407940cdc45
Joe Burgess <josephburgess@gmail.com>
2023-03-29 14:06:14 +0100

    modifying unit test to use sample data

--- frontend/src/components/YearPublishedGraph/YearPublishedGraph.cy.tsx
+++ frontend/src/components/YearPublishedGraph/YearPublishedGraph.cy.tsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import { mount } from '@cypress/react18';
 import YearPublishedGraph from './YearPublishedGraph'
 
 describe('<YearPublishedGraph />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<YearPublishedGraph />)
+
+  beforeEach(() => {
+    cy.fixture('sample_data.json').then((sampleData) => {
+      cy.intercept('GET', '/data', { body: sampleData });
+    });
   })
+
+  it('renders the graph', () => {
+    mount(<YearPublishedGraph />);
+    cy.get('.plot-container.plotly').should('be.visible');
+    cy.get('.legend').should('be.visible');
+  });
 })
