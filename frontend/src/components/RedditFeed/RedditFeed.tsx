45290089bfa37776edcfc2e34e33e50621312b31
Joe Burgess <josephburgess@gmail.com>
2023-04-06 15:38:04 +0100

    adding pagination to reddit feed

--- frontend/src/components/RedditFeed/RedditFeed.tsx
+++ frontend/src/components/RedditFeed/RedditFeed.tsx
@@ -2,16 +2,29 @@ import React, { useState, useEffect } from 'react';
 import RedditPost from '../RedditPost/RedditPost';
 import { fetchRedditPosts } from '../../services/redditApi';
 import Loading from '../Loading/Loading';
+import Paginate from 'react-paginate';
+import styles from './RedditFeed.module.css';
 
 export interface RedditPostData {
   title: string;
   url: string;
   created: number;
+  selftext: string;
 }
 
 const RedditFeed: React.FC = () => {
   const [posts, setPosts] = useState<RedditPostData[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+
+  const handlePageChange = (selectedItem: { selected: number }) => {
+    setCurrentPage(selectedItem.selected);
+  };
+
+  const itemsPerPage = 20;
+  const pageCount = Math.ceil(posts.length / itemsPerPage);
+  const displayPosts = posts.slice(currentPage * itemsPerPage, (currentPage * itemsPerPage) + itemsPerPage);
+
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,11 +39,26 @@ const RedditFeed: React.FC = () => {
   return (
     <div className="reddit-feed">
       <h1>Reddit Feed</h1>
+      <div className={styles['reddit-post-container']}>
       {isLoading ? (
         <Loading />
       ) : (
-        posts.map((post, index) => <RedditPost key={index} {...post} />)
+        displayPosts.map((post, index) => <RedditPost key={index} {...post} />)
       )}
+      </div>
+      <Paginate
+        previousLabel={'previous'}
+        nextLabel={'next'}
+        breakLabel={'...'}
+        breakClassName={'break-me'}
+        pageCount={pageCount}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={5}
+        onPageChange={handlePageChange}
+        containerClassName={styles.pagination}
+        activeClassName={styles.active}
+        disabledClassName={styles.disabled}
+      />
     </div>
   );
 };
