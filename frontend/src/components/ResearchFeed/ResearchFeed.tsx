a1bdd8969eb6ff088af74fe4862a9adc0a88c943
Joe Burgess <josephburgess@gmail.com>
2023-04-07 09:03:08 +0100

    Creating researchfeed component

---
+++ frontend/src/components/ResearchFeed/ResearchFeed.tsx
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { fetchResearchArticleData } from '../../services/dataApi';
+import ResearchArticle from '../ResearchArticle/ResearchArticle';
+import styles from './ResearchFeed.module.css';
+import Loading from '../Loading/Loading';
+
+export interface ResearchArticleData {
+  title:string;
+  source: string;
+  publication_date: string;
+  authors: string;
+  url: string;
+}
+
+const ResearchFeed: React.FC = () => {
+  const [articles, setArticles] = useState<ResearchArticleData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchResearchArticleData();
+      setArticles(data.slice(0, 10));
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className={styles.researchFeed}>
+      {isLoading ? (
+        <Loading />
+      ) : (
+      articles.map((article, index) => <ResearchArticle key={index} {...article} />)
+      )}
+    </div>
+  );
+};
+
+export default ResearchFeed;
+
