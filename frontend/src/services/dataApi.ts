3ffc4bada8ed685e891722834a9dc2fed588141e
Joe Burgess <josephburgess@gmail.com>
2023-04-07 09:14:23 +0100

    add method for fetching resarch data from mongodb

--- frontend/src/services/dataApi.ts
+++ frontend/src/services/dataApi.ts
@@ -1,4 +1,18 @@
 import { YearPublishedArticle } from '../components/YearPublishedGraph/YearPublishedGraph';
+import { ResearchArticleData } from '../components/ResearchFeed/ResearchFeed';
+
+export const fetchResearchArticleData = async (): Promise<
+  ResearchArticleData[]
+> => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/api/data`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
 
 export const fetchYearPublishedArticleData = async (): Promise<
   YearPublishedArticle[]
