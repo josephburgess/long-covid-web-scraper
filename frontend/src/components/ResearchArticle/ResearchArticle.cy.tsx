d80dcabb103ff6e1b6e010481d79454d06833aff
Joe Burgess <josephburgess@gmail.com>
2023-04-07 08:51:14 +0100

    add research article component

---
+++ frontend/src/components/ResearchArticle/ResearchArticle.cy.tsx
@@ -0,0 +1,41 @@
+import { mount } from '@cypress/react18';
+import ResearchArticle from './ResearchArticle'
+
+describe('<ResearchArticle />', () => {
+  const article = {
+    title: 'Test article',
+    source: 'BMJ',
+    publication_date: '2021-Mar-01',
+    authors: 'Test Author, Test Author',
+    url: 'https://www.bmj.com/test-article',
+  };
+
+  beforeEach(() => {
+    mount(<ResearchArticle title={article.title} source={article.source} publication_date={article.publication_date} authors={article.authors} url={article.url} />);
+  });
+
+  it('renders', () => {
+    cy.get('[data-cy="research-article"]').should('be.visible');
+  })
+
+  it('displays the article title', () => {
+    cy.get('[data-cy="title"]').should('contain.text', article.title);
+  })
+
+  it('displays the article URL', () => {
+    cy.get('[data-cy="title"]')
+      .should('have.attr', 'href', article.url);
+  })
+  
+  it('displays the article source', () => {
+    cy.get('[data-cy="source"]').should('contain.text', article.source);
+  })
+
+  it('displays the article publication date', () => {
+    cy.get('[data-cy="publication_date"]').should('contain.text', article.publication_date);
+  })
+
+  it('displays the article authors', () => {
+    cy.get('[data-cy="authors"]').should('contain.text', article.authors);
+  })
+})
---
+++ frontend/src/components/ResearchArticle/ResearchArticle.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './ResearchArticle.module.css';
+
+interface ResearchArticleProps {
+  title: string;
+  source: string;
+  publication_date: string;
+  authors: string;
+  url: string;
+}
+
+const ResearchArticle: React.FC<ResearchArticleProps> = ({
+  title,
+  source,
+  publication_date,
+  authors,
+  url,
+}) => {
+  return (
+    <div data-cy="research-article" className={styles.researchArticle}>
+      <a href={url} data-cy="title" target="_blank" rel="noopener noreferrer">
+        {title}
+      </a>
+      <p data-cy="publication_date">{publication_date}</p>
+      <p data-cy="authors">{authors}</p>
+      <p data-cy="source">{source}</p>
+    </div>
+  );
+};
+
+export default ResearchArticle;
