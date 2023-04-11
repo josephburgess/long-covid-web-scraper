46b18c1cc805afdf206e70dd5c4f8e0e573b0eee
Joe Burgess <josephburgess@gmail.com>
2023-04-11 15:06:18 +0100

    updating styling for researcharticle component

--- frontend/src/components/ResearchArticle/ResearchArticle.tsx
+++ frontend/src/components/ResearchArticle/ResearchArticle.tsx
@@ -16,19 +16,14 @@ const ResearchArticle: React.FC<ResearchArticleProps> = ({
   authors,
   url,
 }) => {
-  // const formattedDate = new Date(publication_date).toLocaleDateString('en-US', {
-  //   year: 'numeric',
-  //   month: 'short',
-  //   day: 'numeric',
-  // });
   return (
     <div data-cy="research-article" className={styles.researchArticle}>
       <a href={url} data-cy="title" target="_blank" rel="noopener noreferrer">
         {title}
       </a>
+      <p data-cy="source">{source}</p>
       <p data-cy="publication_date">{publication_date}</p>
       <p data-cy="authors">{authors}</p>
-      <p data-cy="source">{source}</p>
     </div>
   );
 };
