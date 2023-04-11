150c0f814770c77f2229acc89b118910e9ed4169
Joe Burgess <josephburgess@gmail.com>
2023-04-11 16:12:52 +0100

    updating BMJ scraper to include first page of results

--- backend/src/scrapers/bmj_scraper.py
+++ backend/src/scrapers/bmj_scraper.py
@@ -4,52 +4,47 @@ from bs4 import BeautifulSoup
 
 class BMJScraper(Scraper):
     def __init__(self):
-        self.base_url = 'https://www.bmj.com/search/advanced/'
-        self.query = 'title%3Along%2Bcovid%20title_flags%3Amatch-all%20limit_from%3A1840-01-01%20limit_to%3A2023-03-20%20numresults%3A10%20sort%3Arelevance-rank%20format_result%3Astandard?page='
+        self.base_url = "https://www.bmj.com/search/advanced/"
+        self.query = "title%3Along%2Bcovid%20title_flags%3Amatch-all%20numresults%3A100%20sort%3Apublication-date%20direction%3Adescending%20format_result%3Astandard"
 
     def parse_bmj(self, html_content):
-        soup = BeautifulSoup(html_content, 'html.parser')
+        soup = BeautifulSoup(html_content, "html.parser")
         articles = []
 
-        article_containers = soup.find_all(
-            'div', class_='highwire-article-citation')
+        article_containers = soup.find_all("div", class_="highwire-article-citation")
 
         for container in article_containers:
-            title_tag = container.find(
-                'a', class_='highwire-cite-linked-title')
+            title_tag = container.find("a", class_="highwire-cite-linked-title")
             title = title_tag.text.strip()
-            url = "http://www.bmj.com" + title_tag['href']
+            url = "http://www.bmj.com" + title_tag["href"]
 
             authors_list = container.find_all(
-                'span', {'class': ['nlm-given-names', 'nlm-surname']})
-            authors = ' '.join([author.text for author in authors_list])
+                "span", {"class": ["nlm-given-names", "nlm-surname"]}
+            )
+            authors = " ".join([author.text for author in authors_list])
 
             publication_date = container.find_all(
-                'span', class_='highwire-cite-metadata-date')[1].text.strip()
+                "span", class_="highwire-cite-metadata-date"
+            )[1].text.strip()
 
             article = {
-                'title': title,
-                'url': url,
-                'authors': authors,
-                'publication_date': publication_date,
-                'source': 'BMJ'
+                "title": title,
+                "url": url,
+                "authors": authors,
+                "publication_date": publication_date,
+                "source": "BMJ",
             }
 
             articles.append(article)
 
         return articles
 
-    def scrape(self, max_pages=5):
-        all_articles = []
+    def scrape(self):
+        html_content = self.fetch_html(self.base_url + self.query)
 
-        for page_num in range(1, max_pages + 1):
-            url = f"{self.base_url}{self.query}{page_num}"
-            html_content = self.fetch_html(url)
+        if html_content:
+            articles = self.parse_bmj(html_content)
+        else:
+            articles = []
 
-            if html_content:
-                articles = self.parse_bmj(html_content)
-                all_articles.extend(articles)
-            else:
-                break
-
-        return all_articles
+        return articles
