13230a11d43b622742de3e81fb1237c2a25f34f0
Joe Burgess <josephburgess@gmail.com>
2023-04-18 17:54:11 +0100

    adding tests for db manager, add mongodb to ci-cd workflow

---
+++ backend/tests/database/test_database_manager.py
@@ -0,0 +1,73 @@
+import pytest
+from bson.objectid import ObjectId
+import mongomock
+from src.database import DatabaseManager
+
+
+def mock_get_db():
+    client = mongomock.MongoClient()
+    db = client.test_db
+    return db
+
+
+@pytest.fixture
+def test_db_manager(monkeypatch):
+    monkeypatch.setattr("src.database.get_db", mock_get_db)
+    db_manager = DatabaseManager("test_collection")
+    yield db_manager
+    db_manager.collection.delete_many({})
+
+
+class TestDatabaseManager:
+    def test_existing_article_ids(self, test_db_manager):
+        test_db_manager.collection.insert_one(
+            {"_id": ObjectId(), "title": "Test Article 1"}
+        )
+        test_db_manager.collection.insert_one(
+            {"_id": ObjectId(), "title": "Test Article 2"}
+        )
+
+        existing_ids = test_db_manager._get_existing_article_ids()
+        assert len(existing_ids) == 2
+
+    def test_deduplicate_articles(self, test_db_manager):
+        test_db_manager.collection.insert_one(
+            {"_id": ObjectId(), "title": "Test Article 1"}
+        )
+        test_db_manager.collection.insert_one(
+            {"_id": ObjectId(), "title": "Test Article 2"}
+        )
+
+        new_articles = [
+            {
+                "_id": list(test_db_manager._get_existing_article_ids())[0],
+                "title": "Test Article 1",
+            },
+            {"title": "Test Article 3"},
+        ]
+
+        deduplicated_articles = test_db_manager._deduplicate_articles(new_articles)
+        assert len(deduplicated_articles) == 1
+        assert deduplicated_articles[0]["title"] == "Test Article 3"
+
+    def test_update_collection(self, test_db_manager):
+        new_articles = [
+            {"title": "Test Article 1"},
+            {"title": "Test Article 2"},
+        ]
+
+        test_db_manager.update_collection(new_articles)
+        assert test_db_manager.collection.count_documents({}) == 2
+        assert test_db_manager.new_articles_count == 2
+
+        new_articles = [
+            {
+                "_id": list(test_db_manager._get_existing_article_ids())[0],
+                "title": "Test Article 1",
+            },
+            {"title": "Test Article 3"},
+        ]
+
+        test_db_manager.update_collection(new_articles)
+        assert test_db_manager.collection.count_documents({}) == 3
+        assert test_db_manager.new_articles_count == 1
