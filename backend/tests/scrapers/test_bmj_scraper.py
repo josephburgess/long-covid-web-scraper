9a0b383dd6a04af91d4998eff14de9d771772e17
Joe Burgess <josephburgess@gmail.com>
2023-04-21 12:36:20 +0100

    improving coverage and tidying web scraper tests

--- backend/tests/scrapers/test_bmj_scraper.py
+++ backend/tests/scrapers/test_bmj_scraper.py
@@ -1,30 +1,31 @@
-import unittest
+import pytest
 import responses
 from src.scrapers import BMJScraper
-from .sample_html import sample_bmj_html
+from .mocks import sample_bmj_html, bmj_url
 
 
-class TestBMJScraper(unittest.TestCase):
-    @responses.activate
-    def test_fetch_html_BMJ(self):
-        url = "https://www.bmj.com/search/advanced/title%3Along%2Bcovid"
-        responses.add(responses.GET, url, body=sample_bmj_html, status=200)
-
-        bmj_scraper = BMJScraper()
-        html_content = bmj_scraper.fetch_html(url)
-
-        assert html_content is not None
-        assert "highwire-article-citation" in html_content
-
-    def test_parse_html_BMJ(self):
-        bmj_scraper = BMJScraper()
-        articles = bmj_scraper.parse_html(sample_bmj_html)
-
-        assert len(articles) == 1
-        assert articles[0]["title"] == "Sample Article Title"
-        assert articles[0]["authors"] == "John Doe"
-        assert articles[0]["publication_date"] == "Mar 01, 2023"
+@pytest.fixture
+def bmj_scraper():
+    return BMJScraper()
 
 
-if __name__ == "__main__":
-    unittest.main()
+@responses.activate
+def test_fetch_html_BMJ(bmj_scraper):
+    responses.add(responses.GET, bmj_url, body=sample_bmj_html, status=200)
+
+    html_content = bmj_scraper.fetch_html(bmj_url)
+
+    assert html_content is not None
+    assert "highwire-article-citation" in html_content
+
+
+@responses.activate
+def test_scrape_BMJ(bmj_scraper):
+    responses.add(responses.GET, bmj_url, body=sample_bmj_html, status=200)
+
+    articles = bmj_scraper.scrape()
+
+    assert len(articles) == 1
+    assert articles[0]["title"] == "Sample Article Title"
+    assert articles[0]["authors"] == "John Doe"
+    assert articles[0]["publication_date"] == "Mar 01, 2023"
--- backend/tests/scrapers/test_pubmed_scraper.py
+++ backend/tests/scrapers/test_pubmed_scraper.py
@@ -1,98 +1,77 @@
-import unittest
+import pytest
 import responses
 from src.scrapers import PubMedScraper
-from .sample_html import (
+from .mocks import (
     sample_pubmed_html,
     sample_pubmed_html_no_abstract,
     sample_pubmed_html_multi_para_abstract,
     sample_pubmed_html_no_citation,
+    pubmed_url,
+    mock_summarise_text,
 )
 
 
-def mock_summarise_text(text):
-    return "Sample summary"
+@pytest.fixture
+def pubmed_scraper():
+    return PubMedScraper()
 
 
-class TestPubMedScraper:
-    @responses.activate
-    def test_fetch_html_pubmed(self):
-        url = "https://pubmed.ncbi.nlm.nih.gov/?term=long+covid"
-        responses.add(responses.GET, url, body=sample_pubmed_html, status=200)
+@responses.activate
+def test_fetch_html_pubmed(pubmed_scraper):
+    responses.add(responses.GET, pubmed_url, body=sample_pubmed_html, status=200)
+    html_content = pubmed_scraper.fetch_html(pubmed_url)
 
-        pubmed_scraper = PubMedScraper()
-        html_content = pubmed_scraper.fetch_html(url)
-
-        assert html_content is not None
-        assert "results-article" in html_content
-
-    @responses.activate
-    def test_fetch_html_pubmed_failed_request(self):
-        url = "https://pubmed.ncbi.nlm.nih.gov/?term=long+covid"
-        responses.add(responses.GET, url, status=404)
-
-        pubmed_scraper = PubMedScraper()
-        html_content = pubmed_scraper.fetch_html(url)
-
-        assert html_content is None
-
-    @responses.activate
-    def test_scrape_pubmed(self, monkeypatch):
-        monkeypatch.setattr("src.clients.summarise_text", mock_summarise_text)
-
-        url = "https://pubmed.ncbi.nlm.nih.gov/?term=%28%22long+covid%22%29&filter=simsearch1.fha&filter=pubt.booksdocs&filter=pubt.clinicaltrial&filter=pubt.meta-analysis&filter=pubt.randomizedcontrolledtrial&filter=pubt.review&filter=pubt.systematicreview&format=abstract&sort=date&size=50&page=1"
-        responses.add(responses.GET, url, body=sample_pubmed_html, status=200)
-
-        summary_api_url = "https://api.smrzr.io/v1/summarize?&num_sentences=5"
-        summary_response = {"summary": "Sample summary"}
-        responses.add(
-            responses.POST, summary_api_url, json=summary_response, status=200
-        )
-
-        pubmed_scraper = PubMedScraper()
-        articles = pubmed_scraper.scrape(max_pages=1)
-
-        assert len(articles) == 1
-        assert articles[0]["title"] == "Sample Article Title"
-        assert articles[0]["authors"] == "John Doe, Jane Smith"
-        assert articles[0]["publication_date"] == "Jan 01, 2022"
-        assert articles[0]["abstract"] == "Sample abstract text"
-
-    def test_parse_pubmed_no_abstract(self, monkeypatch):
-        monkeypatch.setattr("src.clients.summarise_text", mock_summarise_text)
-        pubmed_scraper = PubMedScraper()
-        articles = pubmed_scraper.parse_html(sample_pubmed_html_no_abstract)
-
-        assert len(articles) == 1
-        assert articles[0]["title"] == "Sample Article Title"
-        assert articles[0]["authors"] == "John Doe, Jane Smith"
-        assert articles[0]["publication_date"] == "Jan 01, 2022"
-        assert articles[0]["abstract"] == ""
-
-    def test_parse_pubmed_multi_para_abstract(self, monkeypatch):
-        monkeypatch.setattr("src.clients.summarise_text", mock_summarise_text)
-        pubmed_scraper = PubMedScraper()
-        articles = pubmed_scraper.parse_html(sample_pubmed_html_multi_para_abstract)
-
-        assert len(articles) == 1
-        assert articles[0]["title"] == "Sample Article Title"
-        assert articles[0]["authors"] == "John Doe, Jane Smith"
-        assert articles[0]["publication_date"] == "Jan 01, 2022"
-        assert (
-            articles[0]["abstract"]
-            == "Sample abstract text Another paragraph of abstract"
-        )
-
-    def test_parse_pubmed_no_citation(self, monkeypatch):
-        monkeypatch.setattr("src.clients.summarise_text", mock_summarise_text)
-        pubmed_scraper = PubMedScraper()
-        articles = pubmed_scraper.parse_html(sample_pubmed_html_no_citation)
-
-        assert len(articles) == 1
-        assert articles[0]["title"] == "Sample Article Title"
-        assert articles[0]["authors"] == "John Doe, Jane Smith"
-        assert articles[0]["publication_date"] == ""
-        assert articles[0]["abstract"] == "Sample abstract text"
+    assert html_content is not None
+    assert "results-article" in html_content
 
 
-if __name__ == "__main__":
-    unittest.main()
+@responses.activate
+def test_fetch_html_pubmed_failed_request(pubmed_scraper):
+    responses.add(responses.GET, pubmed_url, status=404)
+    html_content = pubmed_scraper.fetch_html(pubmed_url)
+
+    assert html_content is None
+
+
+@responses.activate
+def test_scrape_pubmed(pubmed_scraper, monkeypatch):
+    monkeypatch.setattr("src.clients.summarise_text", mock_summarise_text)
+    responses.add(responses.GET, pubmed_url, body=sample_pubmed_html, status=200)
+
+    summary_api_url = "https://api.smrzr.io/v1/summarize?&num_sentences=5"
+    summary_response = {"summary": "Sample summary"}
+    responses.add(responses.POST, summary_api_url, json=summary_response, status=200)
+
+    articles = pubmed_scraper.scrape(max_pages=1)
+
+    assert len(articles) == 1
+    assert articles[0]["title"] == "Sample Article Title"
+    assert articles[0]["authors"] == "John Doe, Jane Smith"
+    assert articles[0]["publication_date"] == "Jan 01, 2022"
+    assert articles[0]["abstract"] == "Sample abstract text"
+
+
+def test_parse_pubmed_no_abstract(pubmed_scraper, monkeypatch):
+    monkeypatch.setattr("src.clients.summarise_text", mock_summarise_text)
+    articles = pubmed_scraper.parse_html(sample_pubmed_html_no_abstract)
+
+    assert len(articles) == 1
+    assert articles[0]["abstract"] == ""
+
+
+def test_parse_pubmed_multi_para_abstract(pubmed_scraper, monkeypatch):
+    monkeypatch.setattr("src.clients.summarise_text", mock_summarise_text)
+    articles = pubmed_scraper.parse_html(sample_pubmed_html_multi_para_abstract)
+
+    assert len(articles) == 1
+    assert (
+        articles[0]["abstract"] == "Sample abstract text Another paragraph of abstract"
+    )
+
+
+def test_parse_pubmed_no_citation(pubmed_scraper, monkeypatch):
+    monkeypatch.setattr("src.clients.summarise_text", mock_summarise_text)
+    articles = pubmed_scraper.parse_html(sample_pubmed_html_no_citation)
+
+    assert len(articles) == 1
+    assert articles[0]["publication_date"] == ""
