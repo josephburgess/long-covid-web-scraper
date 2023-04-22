5e7c9b2a4449376d490f4ab08e8ffc20d33b75cd
Joe Burgess <josephburgess@gmail.com>
2023-04-22 10:04:28 +0100

    amending research article component to include summaries

--- frontend/src/components/ResearchArticle/ResearchArticle.tsx
+++ frontend/src/components/ResearchArticle/ResearchArticle.tsx
@@ -1,29 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './ResearchArticle.module.css';
+import { ResearchArticleInterface } from '../../types/ResearchArticleInterface';
 
-interface ResearchArticleProps {
-  title: string;
-  source: string;
-  publication_date: string;
-  authors: string;
-  url: string;
-}
-
-const ResearchArticle: React.FC<ResearchArticleProps> = ({
+const ResearchArticle: React.FC<ResearchArticleInterface> = ({
   title,
   source,
   publication_date,
   authors,
   url,
+  summary,
 }) => {
+  const [showFullSummary, setShowFullSummary] = useState(false);
+
+  const toggleSummary = () => {
+    setShowFullSummary(!showFullSummary);
+  };
+
   return (
-    <div data-cy="research-article" className={styles.researchArticle}>
-      <a href={url} data-cy="title" target="_blank" rel="noopener noreferrer">
+    <div data-cy='research-article' className={styles.researchArticle}>
+      <a href={url} data-cy='title' target='_blank' rel='noopener noreferrer'>
         {title}
       </a>
-      <p data-cy="source">{source}</p>
-      <p data-cy="publication_date">{publication_date}</p>
-      <p data-cy="authors">{authors}</p>
+      <p data-cy='source'>{source}</p>
+      <p data-cy='publication_date'>{publication_date}</p>
+      <p data-cy='authors'>{authors}</p>
+      <p data-cy='summary' className={styles.summary}>
+        {showFullSummary ? summary : summary.substring(0, 200)}
+        ...
+        <span className={styles.showMore} onClick={toggleSummary}>
+          {showFullSummary ? 'Show Less' : 'Show More'}
+        </span>
+      </p>
     </div>
   );
 };
